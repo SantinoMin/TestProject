2ae0eca67123a6315921bc9eb9c56a5b50477a7c
SantinoMin <santinomin0381@gmail.com>
2024-02-29 19:20:17 +0900

    test to push.

--- src/main/java/domain/signUp/DemoApplication.java
+++ src/main/java/domain/signUp/DemoApplication.java
@@ -1,4 +1,4 @@
-package test.swagger;
+package domain.signUp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/domain/signUp/repository/SignUpRepository.java
@@ -0,0 +1,8 @@
+package domain.signUp.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SignUpRepository {
+
+}
